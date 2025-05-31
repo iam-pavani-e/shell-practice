ac9ee062db289106b59f1eda6f922605b7ca3948
pavani <yvan@gmail.com>
2025-05-31 11:51:51 +0530

    checking hello-world

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1  # there should be no space before and after =
+PERSON2=$2
+echo "$PERSON1: Hi, how r u"
+echo "$PERSON2: i am fine, how u doing?"
+echo "$PERSON1: yeah, going well,  r u planning to come to home next week"
+echo "$PERSON2:yes, i will come next week mostly"
+echo "$PERSON1: ok, bye will call you later"
