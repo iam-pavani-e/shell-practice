dc3180f46f55908acf7ca120b9374ab7ada3b133
pavani <yvan@gmail.com>
2025-05-31 12:37:50 +0530

    checking hello-world

--- 07-array.sh
+++ 07-array.sh
@@ -2,5 +2,8 @@
 
 #array :: we can store multiple values
 
-MOVIES=("court" "Hit3" "tandel")
+MOVIES=("court" "Hit3" "tandel" "romancham")
 echo "First move: ${MOVIES[0]}"
+echo "fourth movies: ${MOVIES[3]}"
+echo "All movies: ${MOVIES[@]}"
+
