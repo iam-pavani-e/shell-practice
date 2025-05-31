2a2a6be29b25f406160eaba94edb931ed7e4935e
pavani <yvan@gmail.com>
2025-05-31 11:31:58 +0530

    checking hello-world

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "pav: Hi, how r u"
+echo "vani: i am fine, how u doing?"
+echo "pav: yeah, going well,  r u planning to come to home next week"
+echo "vani:yes, i will come next week mostly"
+echo "Pav: ok, bye will call you later"
