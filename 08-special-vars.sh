9053d099b63da7d59b19288e2b89ac548fa1e345
pavani <yvan@gmail.com>
2025-05-31 13:48:02 +0530

    shell-practice

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -6,4 +6,5 @@ echo "current directory : $PWD"
 echo "user running the script: $USER"
 echo "Home directory of user: $HOME"
 echo "PID of the script: $$"
+sleep 10 &
 echo "PID of last command in background: $!"
